f3223c8280edb46718a8b6b566a65517d227eb7d
MarinaBartulov <bartulovmarina1@gmail.com>
2019-11-28 11:36:08 +0100

    Backend for doctor's home page done.

---
+++ src/main/java/team57/project/service/impl/PatientServiceImpl.java
@@ -0,0 +1,28 @@
+package team57.project.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import team57.project.model.Patient;
+import team57.project.repository.PatientRepository;
+import team57.project.service.PatientService;
+
+import java.util.List;
+
+@Service
+public class PatientServiceImpl implements PatientService {
+
+    @Autowired
+    private PatientRepository patientRepostiory;
+
+    @Override
+    public List<Patient> findAll() {
+        return patientRepostiory.findAll();
+    }
+
+    @Override
+    public Patient findOne(Long id) {
+        return patientRepostiory.findById(id).orElse(null);
+    }
+
+
+}
