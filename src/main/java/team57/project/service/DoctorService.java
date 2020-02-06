3d183583d817c6ee6eb680d5fb15292590f953c3
artbluu <43496798+artbluu@users.noreply.github.com>
2020-02-06 02:46:56 +0100

    Update DoctorService.java

--- src/main/java/team57/project/service/DoctorService.java
+++ src/main/java/team57/project/service/DoctorService.java
@@ -29,11 +29,11 @@ public interface DoctorService {
 
     List<DoctorRating> findFreeDoctors(Clinic clinic, AvailableDoctorRequest adr);
     List<AppointmentDTO> findFreeTerms(Long doctorId, AvailableDoctorRequest adr);
-<<<<<<< HEAD
+
     Boolean sendSurgeryAppointment(Long patientId,AppointmentDTO appointmentDTO);
-=======
+
 
     List<DoctorFA> searchForDoctorsExamTypes(Clinic clinic, ExamType examType);
 
->>>>>>> master
+
 }
