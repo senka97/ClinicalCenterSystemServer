5dc9840c0b01092b0f09170504dbca7b23d2962d
jovica.josipovic97@gmail.com <jovica.josipovic97@gmail.com>
2020-02-07 21:07:44 +0100

    Testing]

--- src/test/java/team57/project/TestSuite.java
+++ src/test/java/team57/project/TestSuite.java
@@ -3,15 +3,22 @@ package team57.project;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import team57.project.service.FastAppointmentServiceTest;
-import team57.project.service.MedicalExamServiceTest;
-import team57.project.service.RoomServiceTest;
+import team57.project.repository.*;
+import team57.project.service.*;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         MedicalExamServiceTest.class,
         RoomServiceTest.class,
-        FastAppointmentServiceTest.class
+        FastAppointmentServiceTest.class,
+        ClinicServiceTest.class,
+        DoctorServiceTest.class,
+        ExamTypesServiceTest.class,
+        PatientServiceTest.class,
+        ClinicRepositoryTest.class,
+        DoctorRepositoryTest.class,
+        ExamTypeRepositoryTest.class,
+        TermDoctorRepositoryTest.class
 })
 public class TestSuite {
 
